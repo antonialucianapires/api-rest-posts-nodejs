f6f7e5a8a9584724717128f53821ea8f5c7690c7
antonialucianapires <antonialucianapires@gmail.com>
2020-10-07 18:54:29 -0300

    feat: funcionalidade de inclusão de novo post utilizando o helper para a lógica de incremento no id

--- src/controller/PostController.js
+++ src/controller/PostController.js
@@ -1,16 +1,35 @@
+const { request, response } = require("express");
 const postModel = require("../models/PostModel");
+const helper = require("../helpers/helper")
 
 const getAll = (request, response) => {
     response.status(200).json(postModel);
 }
 
 const getById = (request, response) => {
-    const {id} = request.params;
-    const post = postModel.find(post => post.id == id);
+    let {id} = request.params;
+    let post = postModel.find(post => post.id == id);
     response.json(post);
+}
+
+const newPost = (request, response) => {
+    let { titulo, descricao, autor, origem} = request.body;
+
+    let newPost = {
+        id: helper.obterNovoValor(postModel),
+        titulo: titulo,
+        descricao: descricao,
+        autor: autor,
+        origem: origem
+    }
+
+    postModel.push(newPost);
+
+    response.status(201).json(newPost);
 }
 
 module.exports = {
     getAll,
-    getById
+    getById,
+    newPost
 }
--- src/routes/PostRoutes.js
+++ src/routes/PostRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const PostController = require('../controller/PostController');
 const router = express.Router();
 
 const postController = require("../controller/PostController");
 
 router.get("/posts", postController.getAll);
 router.get("/posts/:id", postController.getById);
+router.post("/posts", postController.newPost);
 
 module.exports = router
